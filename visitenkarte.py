91000563bfac1867f2799941452d9eb576ee0302
iwiitv <123200982+iwiitv@users.noreply.github.com>
2025-10-06 12:17:42 +0200

    Visitenkarte hinzugefügt

---
+++ visitenkarte.py
@@ -0,0 +1,16 @@
+
+# Variablen
+name = "Patrick Tappe"
+ausbildung = "Fachinformatiker für Systemintegration"
+betrieb = "DAA"
+
+# Ausgabe
+print("========================================")
+print("Name:       " + name)
+print("Ausbildung: " + ausbildung)
+print("Betrieb:    " + betrieb)
+print("========================================")
+
+
+
+
