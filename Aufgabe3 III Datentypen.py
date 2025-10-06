1bfd21a76635581898a45e926fc4230b12d895e4
ehssanaldaoode3-glitch <ehssanaldaoode3@gmail.com>
2025-10-06 12:49:57 +0200

    nue aufgabe

--- Aufgabe3 III Datentypen.py
+++
@@ -1,6 +0,0 @@
-netto=float(input(" price der geräte per netto : "))
-wartesteuer =float(input("gGib den waeresteuer  : %  "))
-
-Gesamtpreis=netto * (1+wartesteuer/100)
-
-print(f" Der Gesamt preis inklusive wartesteuer : {Gesamtpreis:.2f} Euro")
