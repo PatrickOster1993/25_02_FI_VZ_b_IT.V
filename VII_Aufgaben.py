91e97bf8b043d1fdffe417386e7e6e7e41738a93
iwiitv <123200982+iwiitv@users.noreply.github.com>
2025-10-13 15:12:10 +0200

    Übungs Aufgaben erstellt

---
+++ VII_Aufgaben.py
@@ -0,0 +1,30 @@
+#Aufgabe VII 1
+print("Bitte registrieren Sie sich zunächst.")
+name = input("Wie heißen Sie? ")
+passwort = input(f"Hey {name}, bitte leg jetzt ein Passwort fest: ")
+
+print("Registrierung erfolgreich! Jetzt bitte einloggen. ")
+
+login_name = input("Benutzername: ")
+login_passwort = input("Passwort: ")
+
+if login_name == name and login_passwort == passwort:
+    print(f"Herzlich Willkommen, {name}!")
+else:
+    print("Falscher Benutzername oder Passwort!")
+
+#Aufgabe VII 2
+zimmerpreis=70
+anzahl_personen=int(input("Mit wievielen Personen werden sie anreisen ? "))
+dauer=int(input("Wie viele Tage werden sie bleiben (Dauer in Tagen) ? "))
+Mwst=1.19
+nettopreis=zimmerpreis*anzahl_personen*dauer
+gesammtpreis=nettopreis*Mwst
+aufschlag=20
+
+if dauer == 1:
+    print(f"Sie zahlen {gesammtpreis+aufschlag:.2f} € ")
+else:
+    print(f"Sie zahlen {gesammtpreis:.2f} € ")
+
+
