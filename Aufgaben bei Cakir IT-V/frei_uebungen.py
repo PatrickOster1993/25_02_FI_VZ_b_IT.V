ac28cc44604d3e487df48a6aac902adb8b0e8103
Marc <marccoenen.umschulung@hotmail.com>
2025-11-21 13:15:18 +0100

    Unsere Test Frage

---
+++ Aufgaben bei Cakir IT-V/frei_uebungen.py
@@ -0,0 +1,59 @@
+#x = 7
+#y = 15
+#if x <= y:
+#   print("zahl ist grösser ")
+#else:
+#    print
+#print(x, y)
+
+#x= 7
+#y= 15
+ 
+#z=(x <= y)
+
+
+#print( x,y,z)
+
+
+#x+=5
+#y-=5
+
+
+#z=(x <= y)
+#print(x,y,z)
+
+
+
+
+erste_zahl=float(input("Gebe deine erste kommazahl ein "))
+zweite_zahl=int(input("Gebe deine Zweite ganzzahl ein"))
+
+erste_ganzzahl=int(erste_zahl)
+
+print (erste_zahl,zweite_zahl,erste_ganzzahl)
+
+ergebnis= erste_ganzzahl*zweite_zahl
+
+print(ergebnis)
+
+
+ergebnis_zwei=float(ergebnis)
+
+print(ergebnis_zwei)
+
+ergebnis_drei= ergebnis_zwei+25.7
+
+print(ergebnis_drei)
+print(erste_zahl)
+print(zweite_zahl)
+
+erste_zahl-=1.5
+zweite_zahl +=2
+
+
+print(erste_zahl)
+print(zweite_zahl)
+
+vergleich=zweite_zahl<=erste_zahl
+
+print(vergleich)
